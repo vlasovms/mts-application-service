c2719f704b829810b49e708f73c820435526f429
vlasovms <anast.mir22@yandex.ru>
2024-07-18 03:34:32 +0300

    initial commit

---
+++ src/main/java/com/example/java_mts_homework_final/JavaMtsHomeworkFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_mts_homework_final;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMtsHomeworkFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMtsHomeworkFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_mts_homework_final/JavaMtsHomeworkFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_mts_homework_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMtsHomeworkFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
